12d30ec488a2e4303f83fe3c870d6d7a8118ee10
Greg <halfskye@gmail.com>
2017-04-23 16:30:15 -0700

    Fixed Olive spinning based on RigidBody speed/velocity

--- Assets/Scripts/Enemies/Olive.cs
+++ Assets/Scripts/Enemies/Olive.cs
@@ -1,14 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Olive : MonoBehaviour {
 
 	[SerializeField]
 	private float _rotateSpeed = 1.0f;
 
-	// Update is called once per frame
-	private void Update () {
-		this.transform.RotateAround(this.transform.position, Vector3.forward, _rotateSpeed * Time.deltaTime);
+	private Rigidbody2D _rigidBody = null;
+
+	private void Awake() {
+		_rigidBody = this.GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate() {
+		float speed = _rigidBody.velocity.sqrMagnitude;
+		float direction = GetDirection(_rigidBody.velocity);
+		this.transform.RotateAround(this.transform.position, Vector3.forward, _rotateSpeed * speed * Time.deltaTime * direction);
+	}
+
+	private float GetDirection(Vector3 velocity) {
+		float dotLeft = Vector3.Dot(velocity, Vector3.left);
+		float dotRight = Vector3.Dot(velocity, Vector3.right);
+		return (dotLeft > dotRight) ? 1.0f : -1.0f;
 	}
 }
