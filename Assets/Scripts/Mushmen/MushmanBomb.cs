65e1d542e6d54c33132d310b2ffc735f511e18c0
Greg <halfskye@gmail.com>
2017-04-22 20:15:49 -0700

    Basic Mushman Bomb detonation

--- Assets/Scripts/Mushmen/MushmanBomb.cs
+++ Assets/Scripts/Mushmen/MushmanBomb.cs
@@ -4,7 +4,30 @@ using UnityEngine;
 
 public class MushmanBomb : MushmanBase {
 
+	[SerializeField]
+	private float _damageRadius = 10.0f;
+
 	override protected void AttackTarget(Enemy enemy) {
-		base.AttackTarget(enemy);
+		Detonate();
   }
+
+	private void Detonate() {
+		// Find all Enemies within its radius.
+		bool foundEnemy = false;
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(this.transform.position, _damageRadius);
+		if(colliders != null && colliders.Length > 0) {
+			Debug.Log("We Found some Colliders");
+		}
+		foreach(Collider2D collider in colliders) {
+			if(collider.tag == "Enemy") {
+				foundEnemy = true;
+				Enemy enemy = collider.GetComponent<Enemy>();
+				base.AttackTarget(enemy);
+			}
+		}
+
+		if(foundEnemy) {
+			Destroy(this.gameObject);
+		}
+	}
 }
