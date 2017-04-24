8b7824f8bdf4a05dd9349c7c4957427f9bf5b6a5
Joshua Watson <josh.m.watson@gmail.com>
2017-04-24 17:06:43 -0500

    Wible Wobble Restart and Exit

--- Assets/Scripts/WinnerScenes/ExitButton.cs
+++ Assets/Scripts/WinnerScenes/ExitButton.cs
@@ -1,16 +1,46 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Scripts.WinnerScenes
 {
     public class ExitButton : MonoBehaviour
     {
+        private float _originalY;
+        public float FloatSpeed = 6;
+        public float FloatStrength = 0.4f;
+
+        private bool _isHovering;
+
+        void Start()
+        {
+            _originalY = transform.position.y;
+        }
+
+        void Update()
+        {
+        }
+
         public void OnMouseOver()
         {
+            if (_isHovering)
+            {
+                transform.position = new Vector3(transform.position.x, _originalY + ((float)Math.Sin(Time.time * FloatSpeed) * FloatStrength), transform.position.z);
+            }
             if (Input.GetMouseButtonDown(0))
             {
                 Debug.Log("The Game Should be Closed!");
                 Application.Quit();
             }
+        }
+
+        void OnMouseEnter()
+        {
+            _isHovering = true;
+        }
+
+        void OnMouseExit()
+        {
+            _isHovering = false;
         }
     }
 }
--- Assets/Scripts/WinnerScenes/RestartButton.cs
+++ Assets/Scripts/WinnerScenes/RestartButton.cs
@@ -1,16 +1,42 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.WinnerScenes
 {
     public class RestartButton :MonoBehaviour
     {
+        private float _originalY;
+        public float FloatSpeed = 6;
+        public float FloatStrength = 0.04f;
+
+        private bool _isHovering;
+
+        void Start()
+        {
+            _originalY = transform.position.y;
+        }
+
         public void OnMouseOver()
         {
+            if (_isHovering)
+            {
+                transform.position = new Vector3(transform.position.x, _originalY + ((float)Math.Sin(Time.time * FloatSpeed) * FloatStrength), transform.position.z);
+            }
             if (Input.GetMouseButtonDown(0))
             {
                 SceneManager.LoadScene(Constants.Scenes.TitleScene);
             }
+        }
+
+        void OnMouseEnter()
+        {
+            _isHovering = true;
+        }
+
+        void OnMouseExit()
+        {
+            _isHovering = false;
         }
     }
 }
