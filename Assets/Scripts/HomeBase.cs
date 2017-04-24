f9a2118b1fc2e7acc0da3394c6eccc979dbe748f
Greg <halfskye@gmail.com>
2017-04-24 15:32:09 -0700

    Hooked up Win/Lose HomeBase Anims/State

--- Assets/Scripts/HomeBase.cs
+++ Assets/Scripts/HomeBase.cs
@@ -21,24 +21,46 @@ public class HomeBase : MonoBehaviour {
 	[SerializeField]
 	private TextMesh _timerText = null;
 
+	[SerializeField]
+	private RuntimeAnimatorController _animWin = null;
+	[SerializeField]
+	private RuntimeAnimatorController _animLose = null;
+
+	private Animator _anim = null;
+
+	private enum HomeBaseState {
+		NORMAL = 0,
+		WIN = 1,
+		LOSE = 2,
+	};
+	private HomeBaseState _state = HomeBaseState.NORMAL;
+	private float _stateTimer = 0.0f;
+	private bool IsNormalPlay() { return (_state == HomeBaseState.NORMAL); }
+
 	public bool HasEnoughResource(float amount) {
 		return (amount <= _resource);
 	}
 
 	public void AddResource(float amount) {
+		if(!IsNormalPlay()) return;
+
 		_resource += amount;
 	}
 
 	public void UseResource(float amount) {
+		if(!IsNormalPlay()) return;
+
 		_resource -= amount;
 	}
 
 	public void Damage(float amount) {
+		if(!IsNormalPlay()) return;
+
 		_resource -= amount;
 
 		// Death Scenario:
 		if(_resource <= 0.0f) {
-			Die();
+			SetLoseState();
 		}
 	}
 
@@ -46,18 +68,58 @@ public class HomeBase : MonoBehaviour {
 		_singleton = this;
 		_resource = STARTING_RESOURCE;
 		_timer = STARTING_TIME;
+		_state = HomeBaseState.NORMAL;
+		_anim = this.GetComponent<Animator>();
 	}
 
 	private void Update()
 	{
+		UpdateState();
+	}
+
+	private void UpdateState() {
+		switch(_state) {
+			case HomeBaseState.NORMAL:
+			UpdateNormalState();
+			break;
+			case HomeBaseState.WIN:
+			UpdateWinState();
+			break;
+			case HomeBaseState.LOSE:
+			UpdateLoseState();
+			break;
+		}
+	}
+
+	private void UpdateNormalState() {
 		Timer();
 	}
 
-	private void Die() {
-		SceneManager.LoadScene(Constants.Scenes.GameOver);
+	private void SetWinState() {
+		_state = HomeBaseState.WIN;
+		_anim.runtimeAnimatorController = _animWin;
+		_stateTimer = _anim.GetCurrentAnimatorStateInfo(0).length;
+	}
 
-		//@TODO: Die Mofucka.
-		GameObject.Destroy(this.gameObject);
+	private void UpdateWinState() {
+		_stateTimer -= Time.deltaTime;
+		if(_stateTimer <= 0.0f) {
+			SceneManager.LoadScene(Constants.Scenes.Winner1);
+		}
+	}
+
+	private void SetLoseState() {
+		_state = HomeBaseState.LOSE;
+		_resource = 0.0f;
+		_anim.runtimeAnimatorController = _animLose;
+		_stateTimer = _anim.GetCurrentAnimatorStateInfo(0).length;
+	}
+
+	private void UpdateLoseState() {
+		_stateTimer -= Time.deltaTime;
+		if(_stateTimer <= 0.0f) {
+			SceneManager.LoadScene(Constants.Scenes.GameOver);
+		}
 	}
 
 	private void OnGUI()
@@ -75,7 +137,7 @@ public class HomeBase : MonoBehaviour {
 		_timer -= Time.deltaTime;
 		if (_timer <= 0f)
 		{
-			SceneManager.LoadScene(Constants.Scenes.Winner1);
+			SetWinState();
 		}
 	}
 }
