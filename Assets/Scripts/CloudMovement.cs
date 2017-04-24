90f1c22bb57669461217c92f5da3e16a3a0bcaa8
Joshua Watson <josh.m.watson@gmail.com>
2017-04-23 19:55:48 -0500

    added cloud prefabs and Hotbar prefabs

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CloudMovement : MonoBehaviour
+    {
+
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/CloudSpawner.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CloudSpawner : MonoBehaviour
+    {
+        public GameObject[] Clouds;
+        
+        //Time to spawn
+        public float WaitForNext = 10;
+        public float CountDown = 10;
+
+        //X Range
+        public float XMin;
+        public float XMax;
+
+        //Y Range
+        public float YMin;
+        public float YMax;
+
+        void Update()
+        {
+            CountDown -= Time.deltaTime;
+            if (CountDown <= 0)
+            {
+                SpawnCloud();
+                CountDown = WaitForNext;
+            }
+        }
+
+        private void SpawnCloud()
+        {
+            var pos = new Vector2(Random.Range(XMin,XMax), Random.Range(YMin,YMax));
+            var enemyPrefab = Clouds[Random.Range(0,Clouds.Length)];
+
+            Instantiate(enemyPrefab, pos, transform.rotation);
+        }
+    }
+}
