555aa19cd752c5b1dac65b5a354922983159516e
Joshua Watson <josh.m.watson@gmail.com>
2017-04-23 23:17:55 -0500

    Instrucstion Scene

--- Assets/Scripts/TitleController.cs
+++ Assets/Scripts/TitleController.cs
@@ -6,12 +6,20 @@ namespace Assets.Scripts
     public class TitleController : MonoBehaviour
     {
         private bool SPACE_UP() { return (Input.GetKey(KeyCode.Space)); }
+        private Scene _scene;
 
         void Update()
         {
+            _scene = SceneManager.GetActiveScene();
+
             if (SPACE_UP())
             {
-                SceneManager.LoadScene(Constants.Scenes.InstructionsScene);
+                if (_scene.name == Constants.Scenes.TitleScene)
+                    SceneManager.LoadScene(Constants.Scenes.InstructionsScene);
+                else if (_scene.name == Constants.Scenes.InstructionsScene)
+                {
+                    SceneManager.LoadScene(Constants.Scenes.GameScene);
+                }
             }
         }
     }
