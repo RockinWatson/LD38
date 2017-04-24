ddc9b2267ece4d89abc6023e0ecff0e2dfa57ac4
Joshua Watson <josh.m.watson@gmail.com>
2017-04-23 20:14:57 -0500

    clouds and stuff

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -4,10 +4,16 @@ namespace Assets.Scripts
 {
     public class CloudMovement : MonoBehaviour
     {
+        private bool _randomSpeed;
+
+        void Start()
+        {
+            //_randomSpeed = Random.Range(0.1, 0.9);
+        }
 
         void Update()
         {
-
+            transform.Translate(Vector2.right * 0.1f);
         }
     }
 }
