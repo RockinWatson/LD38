11fd1b4c760367b869a0698ddeb756c480cb107f
Joshua Watson <josh.m.watson@gmail.com>
2017-04-24 09:50:12 -0500

    Winner1 animations

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -18,7 +18,7 @@
             public const string GameOver = "GameOver";
             public const string TitleScene = "TitleScene";
             public const string Winner1 = "Winner1";
-            public const string Winner2 = "Winners";
+            public const string Winner2 = "Winner2";
             public const string GameScene = "TestScene1";
 
             public enum SceneType {
---
+++ Assets/Scripts/WinnerScenes/PizzaFly.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.WinnerScenes
+{
+    public class PizzaFly : MonoBehaviour
+    {
+        void Update()
+        {
+            transform.Translate(Vector2.up * 0.05f);
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag(Constants.Tags.NextSceneTrigger))
+            {
+                Destroy(this.gameObject);
+                SceneManager.LoadScene(Constants.Scenes.Winner2);
+            }
+        }
+    }
+}
