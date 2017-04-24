ded78adc15c2378ff3b18a13a25ec8381b08c2b1
Joshua Watson <josh.m.watson@gmail.com>
2017-04-24 12:29:32 -0500

    Timer Added for gameplay

--- Assets/Scripts/HomeBase.cs
+++ Assets/Scripts/HomeBase.cs
@@ -13,6 +13,9 @@ public class HomeBase : MonoBehaviour {
 
 	private float _resource;
 
+    public Font MyFont;
+    public float timer;
+
 	public bool HasEnoughResource(float amount) {
 		return (amount <= _resource);
 	}
@@ -39,7 +42,12 @@ public class HomeBase : MonoBehaviour {
 		_resource = STARTING_RESOURCE;
 	}
 
-	private void Die() {
+    void Update()
+    {
+        Timer();
+    }
+
+    private void Die() {
 		SceneManager.LoadScene(Constants.Scenes.GameOver);
 
 		//@TODO: Die Mofucka.
@@ -51,13 +59,36 @@ public class HomeBase : MonoBehaviour {
 		if(Application.isEditor) {
 			//Rect location = new Rect(Screen.width / 2, Screen.height - 10, Screen.width, Screen.height);
 			GUIStyle style = new GUIStyle();
-		    style.font = Font.CreateDynamicFontFromOSFont("Cheeseburger",30);
+		    GUI.skin.font = MyFont;
+		    style.fontSize = 35;
 			//style.alignment = TextAnchor.LowerCenter;
 			//style.normal.textColor = Color.black;
-			float posX = (Screen.width / 2) + 100;
-			float posY = Screen.height - 135;
+			float posX = (Screen.width / 2) + 40;
+			float posY = Screen.height - 139;
 			Rect location = new Rect(posX, posY, Screen.width, Screen.height);
 			GUI.Label(location, "" + _resource, style);
-		}
+
+            //Timer
+		    GUIStyle style2 = new GUIStyle();
+		    GUI.skin.font = MyFont;
+		    style.fontSize = 35;
+		    float posX2 = (Screen.width / 2) + 150;
+		    float posY2 = Screen.height - 139;
+		    Rect location2 = new Rect(posX2, posY2, Screen.width, Screen.height);
+
+		    int minutes = Mathf.FloorToInt(timer / 60F);
+		    int seconds = Mathf.FloorToInt(timer - minutes * 60);
+		    string niceTime = string.Format("{0:0}:{1:00}", minutes, seconds);
+            GUI.Label(location2, "" + niceTime, style);
+        }
 	}
+
+    private void Timer()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0f)
+        {
+            SceneManager.LoadScene(Constants.Scenes.Winner1);   
+        }
+    }
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class Timer : MonoBehaviour
+    {
+        public float timeLeft = 180.0f;
+        public bool stop = true;
+
+        private float _minutes;
+        private float _seconds;
+
+        public Text text;
+
+        public void StartTimer(float from)
+        {
+            timeLeft = from;
+            Update();
+            StartCoroutine(UpdateCoroutine());
+        }
+
+        void Update()
+        {
+            if (stop) return;
+            timeLeft -= Time.deltaTime;
+
+            _minutes = Mathf.Floor(timeLeft / 60);
+            _seconds = timeLeft % 60;
+            if (_seconds > 59) _seconds = 59;
+            if (_minutes < 0)
+            {
+                stop = true;
+                _minutes = 0;
+                _seconds = 0;
+            }
+        }
+
+        private IEnumerator UpdateCoroutine()
+        {
+            while (!stop)
+            {
+                text.text = string.Format("{0:0}:{1:00}", _minutes, _seconds);
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+    }
+}
