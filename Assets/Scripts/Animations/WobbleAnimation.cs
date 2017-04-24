7af4a14ed35ad2d727a526986b3c91fb57143cce
Joshua Watson <josh.m.watson@gmail.com>
2017-04-24 15:08:51 -0500

    Cloud Floats.

--- Assets/Scripts/Animations/WobbleAnimation.cs
+++ Assets/Scripts/Animations/WobbleAnimation.cs
@@ -1,9 +1,22 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
     public class WobbleAnimation : MonoBehaviour
     {
+        private Vector2 _floatY;
+        private float _originalY;
+        public float FloatStrength;
 
+        void Start()
+        {
+            _originalY = transform.position.y;
+        }
+
+        void Update()
+        {
+            transform.position = new Vector3(transform.position.x, _originalY + ((float)Math.Sin(Time.time) * FloatStrength), transform.position.z);
+        }
     }
 }
